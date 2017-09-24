55109871a5b29b9c712a374aaf944311a4622442
Portugao <info@homepages-mit-zikula.de>
2017-09-24 17:50:28 +0200

    typo

--- MU/NewsModule/Entity/Repository/MessageRepository.php
+++ MU/NewsModule/Entity/Repository/MessageRepository.php
@@ -21,5 +21,5 @@ use MU\NewsModule\Entity\Repository\Base\AbstractMessageRepository;
  */
 class MessageRepository extends AbstractMessageRepository
 {
-
+    // feel free to add your own methods here, like for example reusable DQL queries
 }
