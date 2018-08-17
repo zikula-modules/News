379424802747037f544c997b628bc2dd5f8b06a5
Michael Ueberschaer <info@homepages-mit-zikula.de>
2018-08-17 08:35:47 +0200

    fixed #40

--- MU/NewsModule/Helper/ModelHelper.php
+++ MU/NewsModule/Helper/ModelHelper.php
@@ -19,5 +19,43 @@ use MU\NewsModule\Helper\Base\AbstractModelHelper;
  */
 class ModelHelper extends AbstractModelHelper
 {
+    /**
+     * Returns a desired sorting criteria for passing it to a repository method.
+     *
+     * @param string $objectType Name of treated entity type
+     * @param string $sorting    The type of sorting (newest, random, default)
+     *
+     * @return string The order by clause
+     */
+    public function resolveSortParameter($objectType = '', $sorting = 'default')
+    {
+        if ($sorting == 'random') {
+            return 'RAND()';
+        }
+        
+        $hasStandardFields = in_array($objectType, ['message']);
+        
+        $sortParam = '';
+        if ($sorting == 'newest') {
+            if (true === $hasStandardFields) {
+                $sortParam = 'createdDate DESC';
+            } else {
+                $sortParam = $this->entityFactory->getIdField($objectType) . ' DESC';
+            }
+        } elseif ($sorting == 'updated') {
+            if (true === $hasStandardFields) {
+                $sortParam = 'updatedDate DESC';
+            } else {
+                $sortParam = $this->entityFactory->getIdField($objectType) . ' DESC';
+            }
+        } elseif ($sorting == 'default') {
+            $repository = $this->entityFactory->getRepository($objectType);
+            $sortParam = $repository->getDefaultSortingField();
+        } elseif ($sorting == 'startedlast') {
+            $sortParam = 'startDate DESC';
+        }
+        
+        return $sortParam;
+    }
     // feel free to add your own convenience methods here
 }
