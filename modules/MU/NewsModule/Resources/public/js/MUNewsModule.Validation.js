4dfd66c1b07d114327d4a56c63d488f16884ebc7
Guite <Guite@users.noreply.github.com>
2020-01-12 12:35:53 +0000

    update generated code :rocket:

--- modules/MU/NewsModule/Resources/public/js/MUNewsModule.Validation.js
+++ modules/MU/NewsModule/Resources/public/js/MUNewsModule.Validation.js
@@ -111,10 +111,10 @@ function mUNewsExecuteCustomValidationConstraints(objectType, currentEntityId) {
             if ('DIV' == jQuery(this).prop('tagName')) {
                 if (!mUNewsValidateDateRangeMessage()) {
                     jQuery('#' + jQuery(this).attr('id') + '_date').get(0).setCustomValidity(Translator.__('The start must be before the end.'));
-                    jQuery('#' + jQuery(this).attr('id') + '_time').setCustomValidity(Translator.__('The start must be before the end.'));
+                    jQuery('#' + jQuery(this).attr('id') + '_time').get(0).setCustomValidity(Translator.__('The start must be before the end.'));
                 } else {
                     jQuery('#' + jQuery(this).attr('id') + '_date').get(0).setCustomValidity('');
-                    jQuery('#' + jQuery(this).attr('id') + '_time').setCustomValidity('');
+                    jQuery('#' + jQuery(this).attr('id') + '_time').get(0).setCustomValidity('');
                 }
             } else {
                 if (!mUNewsValidateDateRangeMessage()) {
